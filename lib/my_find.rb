f053ba3787b626661772c92017bc7c1ad65fc6b0
Emily Jones <emjones829@gmail.com>
2020-06-07 19:05:35 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  while 1 < collection.length return colection[i] if yield(collection[i])
+  while 1 < collection.length
+    return colection[i] if yield(collection[i])
     i = i + 1
   end
 end
